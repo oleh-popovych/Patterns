49ad05b72578b021bb8b6b4e08044cbf02584fdd
Oleh Popovych <oleh.popovych@pettersonapps.com>
2023-01-03 17:42:38 +0200

    Add my variant of Factory Pattern.

---
+++ My Tries/Factory.ts
@@ -0,0 +1,462 @@
+const openseaResponse = {
+    "id": 158831,
+    "num_sales": 2,
+    "background_color": null,
+    "image_url": "https://i.seadn.io/gcs/files/f3564ef33373939b024fb791f21ec37b.png?w=500&auto=format",
+    "image_preview_url": "https://i.seadn.io/gcs/files/f3564ef33373939b024fb791f21ec37b.png?w=500&auto=format",
+    "image_thumbnail_url": "https://i.seadn.io/gcs/files/f3564ef33373939b024fb791f21ec37b.png?w=500&auto=format",
+    "image_original_url": "https://www.larvalabs.com/cryptopunks/cryptopunk1.png",
+    "animation_url": null,
+    "animation_original_url": null,
+    "name": "CryptoPunk #1",
+    "description": null,
+    "external_link": "https://www.larvalabs.com/cryptopunks/details/1",
+    "asset_contract": {
+        "address": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+        "asset_contract_type": "non-fungible",
+        "created_date": "2018-01-23T04:51:38.832339",
+        "name": "CryptoPunks",
+        "nft_version": "unsupported",
+        "opensea_version": null,
+        "owner": null,
+        "schema_name": "CRYPTOPUNKS",
+        "symbol": "PUNK",
+        "total_supply": null,
+        "description": "CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard. They have been featured in places like The New York Times, Christie’s of London, Art|Basel Miami, and The PBS NewsHour.",
+        "external_link": "https://www.larvalabs.com/cryptopunks",
+        "image_url": "https://i.seadn.io/gae/BdxvLseXcfl57BiuQcQYdJ64v-aI8din7WPk0Pgo3qQFhAUH-B6i-dCqqc_mCkRIzULmwzwecnohLhrcH8A9mpWIZqA7ygc52Sr81hE?w=500&auto=format",
+        "default_to_fiat": false,
+        "dev_buyer_fee_basis_points": 0,
+        "dev_seller_fee_basis_points": 0,
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": 0,
+        "opensea_seller_fee_basis_points": 250,
+        "buyer_fee_basis_points": 0,
+        "seller_fee_basis_points": 250,
+        "payout_address": null
+    },
+    "permalink": "https://opensea.io/assets/ethereum/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/1",
+    "collection": {
+        "payment_tokens": [
+            {
+                "id": 13689077,
+                "symbol": "ETH",
+                "address": "0x0000000000000000000000000000000000000000",
+                "image_url": "https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg",
+                "name": "Ether",
+                "decimals": 18,
+                "eth_price": 1.0,
+                "usd_price": 1218.49
+            },
+            {
+                "id": 12182941,
+                "symbol": "DAI",
+                "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+                "image_url": "https://openseauserdata.com/files/dai-ethereum.svg",
+                "name": "Dai Stablecoin",
+                "decimals": 18,
+                "eth_price": 0.00082184,
+                "usd_price": 1.001
+            },
+            {
+                "id": 4645681,
+                "symbol": "WETH",
+                "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+                "image_url": "https://openseauserdata.com/files/accae6b6fb3888cbff27a013729c22dc.svg",
+                "name": "Wrapped Ether",
+                "decimals": 18,
+                "eth_price": 1.0,
+                "usd_price": 1218.49
+            },
+            {
+                "id": 4403908,
+                "symbol": "USDC",
+                "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+                "image_url": "https://openseauserdata.com/files/749015f009a66abcb3bbb3502ae2f1ce.svg",
+                "name": "USD Coin",
+                "decimals": 6,
+                "eth_price": 0.00082102,
+                "usd_price": 1.0
+            }
+        ],
+        "primary_asset_contracts": [
+            {
+                "address": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+                "asset_contract_type": "non-fungible",
+                "created_date": "2018-01-23T04:51:38.832339",
+                "name": "CryptoPunks",
+                "nft_version": "unsupported",
+                "opensea_version": null,
+                "owner": null,
+                "schema_name": "CRYPTOPUNKS",
+                "symbol": "PUNK",
+                "total_supply": null,
+                "description": "CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard. They have been featured in places like The New York Times, Christie’s of London, Art|Basel Miami, and The PBS NewsHour.",
+                "external_link": "https://www.larvalabs.com/cryptopunks",
+                "image_url": "https://i.seadn.io/gae/BdxvLseXcfl57BiuQcQYdJ64v-aI8din7WPk0Pgo3qQFhAUH-B6i-dCqqc_mCkRIzULmwzwecnohLhrcH8A9mpWIZqA7ygc52Sr81hE?w=500&auto=format",
+                "default_to_fiat": false,
+                "dev_buyer_fee_basis_points": 0,
+                "dev_seller_fee_basis_points": 0,
+                "only_proxied_transfers": false,
+                "opensea_buyer_fee_basis_points": 0,
+                "opensea_seller_fee_basis_points": 250,
+                "buyer_fee_basis_points": 0,
+                "seller_fee_basis_points": 250,
+                "payout_address": null
+            }
+        ],
+        "traits": {},
+        "stats": {
+            "one_hour_volume": 0.0,
+            "one_hour_change": 0.0,
+            "one_hour_sales": 0.0,
+            "one_hour_sales_change": 0.0,
+            "one_hour_average_price": 0.0,
+            "one_hour_difference": 0.0,
+            "six_hour_volume": 169.69,
+            "six_hour_change": -0.45820561051368713,
+            "six_hour_sales": 1.0,
+            "six_hour_sales_change": -0.75,
+            "six_hour_average_price": 0.0,
+            "six_hour_difference": -143.51,
+            "one_day_volume": 758.98,
+            "one_day_change": 1.224769115447998,
+            "one_day_sales": 9.0,
+            "one_day_sales_change": 0.800000011920929,
+            "one_day_average_price": 84.33111111111111,
+            "one_day_difference": 417.83000000000004,
+            "seven_day_volume": 5798.189999999997,
+            "seven_day_change": 1.3798216128086505,
+            "seven_day_sales": 67.0,
+            "seven_day_average_price": 86.5401492537313,
+            "seven_day_difference": 3361.7930999999967,
+            "thirty_day_volume": 13193.426900000004,
+            "thirty_day_change": -0.12555313784569463,
+            "thirty_day_sales": 184.0,
+            "thirty_day_average_price": 71.70340706521742,
+            "thirty_day_difference": -1894.3131000000012,
+            "total_volume": 1084139.503909339,
+            "total_sales": 22171.0,
+            "total_supply": 9998.0,
+            "count": 9998.0,
+            "num_owners": 3663,
+            "average_price": 48.898989847518784,
+            "num_reports": 1,
+            "market_cap": 865228.4122388056,
+            "floor_price": 0
+        },
+        "banner_image_url": "https://i.seadn.io/gae/48oVuDyfe_xhs24BC2TTVcaYCX7rrU5mpuQLyTgRDbKHj2PtzKZsQ5qC3xTH4ar34wwAXxEKH8uUDPAGffbg7boeGYqX6op5vBDcbA?w=500&auto=format",
+        "chat_url": null,
+        "created_date": "2017-06-23T20:55:07.000000+00:00",
+        "default_to_fiat": false,
+        "description": "CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard. They have been featured in places like The New York Times, Christie’s of London, Art|Basel Miami, and The PBS NewsHour.",
+        "dev_buyer_fee_basis_points": "0",
+        "dev_seller_fee_basis_points": "0",
+        "discord_url": "https://discord.gg/tQp4pSE",
+        "display_data": {
+            "card_display_style": "cover"
+        },
+        "external_url": "https://www.larvalabs.com/cryptopunks",
+        "featured": false,
+        "featured_image_url": "https://i.seadn.io/gcs/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb-featured-1556589448.png?w=500&auto=format",
+        "hidden": false,
+        "safelist_request_status": "verified",
+        "image_url": "https://i.seadn.io/gae/BdxvLseXcfl57BiuQcQYdJ64v-aI8din7WPk0Pgo3qQFhAUH-B6i-dCqqc_mCkRIzULmwzwecnohLhrcH8A9mpWIZqA7ygc52Sr81hE?w=500&auto=format",
+        "is_subject_to_whitelist": false,
+        "large_image_url": "https://i.seadn.io/gae/QB2kKuQEw04X02V9EoC2BNYZV652LYuewUv9ZdR7KJfI9Jocwmd28jIfsGg0umSCr2bOMV8O9UpLAkoaqfYwvwmC?w=500&auto=format",
+        "medium_username": null,
+        "name": "CryptoPunks",
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": "0",
+        "opensea_seller_fee_basis_points": "250",
+        "payout_address": null,
+        "require_email": false,
+        "short_description": null,
+        "slug": "cryptopunks",
+        "telegram_url": null,
+        "twitter_username": "larvalabs",
+        "instagram_username": null,
+        "wiki_url": null,
+        "is_nsfw": false,
+        "fees": {
+            "seller_fees": {},
+            "opensea_fees": {
+                "0x0000a26b00c1f0df003000390027140000faa719": 250
+            }
+        },
+        "is_rarity_enabled": false
+    },
+    "decimals": null,
+    "token_metadata": null,
+    "is_nsfw": false,
+    "owner": null,
+    "seaport_sell_orders": null,
+    "creator": {
+        "user": null,
+        "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/22.png",
+        "address": "0xc352b534e8b987e036a93539fd6897f53488e56a",
+        "config": ""
+    },
+    "traits": [
+        {
+            "trait_type": "type",
+            "value": "Male",
+            "display_type": null,
+            "max_value": null,
+            "trait_count": 6039,
+            "order": null
+        },
+        {
+            "trait_type": "accessory",
+            "value": "Mohawk",
+            "display_type": null,
+            "max_value": null,
+            "trait_count": 441,
+            "order": null
+        },
+        {
+            "trait_type": "accessory",
+            "value": "Smile",
+            "display_type": null,
+            "max_value": null,
+            "trait_count": 238,
+            "order": null
+        },
+        {
+            "trait_type": "accessory",
+            "value": "2 attributes",
+            "display_type": null,
+            "max_value": null,
+            "trait_count": 3279,
+            "order": null
+        }
+    ],
+    "last_sale": {
+        "asset": {
+            "decimals": null,
+            "token_id": "1"
+        },
+        "asset_bundle": null,
+        "event_type": "successful",
+        "event_timestamp": "2020-11-30T18:44:26",
+        "auction_type": null,
+        "total_price": "60000000000000000000",
+        "payment_token": {
+            "symbol": "ETH",
+            "address": "0x0000000000000000000000000000000000000000",
+            "image_url": "https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg",
+            "name": "Ether",
+            "decimals": 18,
+            "eth_price": "1.000000000000000",
+            "usd_price": "1218.490000000000009000"
+        },
+        "transaction": {
+            "block_hash": "0xe3eacbc6f4d6bb43525b69baffe09477f452f0f5e1a5b1d5232dc23b6cb176cb",
+            "block_number": "11361817",
+            "from_account": {
+                "user": {
+                    "username": "GoWestBTC"
+                },
+                "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/4.png",
+                "address": "0xee3766e4f996dc0e0f8c929954eaafef3441de87",
+                "config": ""
+            },
+            "id": 63641091,
+            "timestamp": "2020-11-30T18:44:26",
+            "to_account": {
+                "user": null,
+                "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/23.png",
+                "address": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+                "config": ""
+            },
+            "transaction_hash": "0xf4af5563f3c4c3b26dee3ab027902f113bee5985b28d9ede5b81ab42b46abb30",
+            "transaction_index": "58"
+        },
+        "created_date": "2020-11-30T18:45:11.476313",
+        "quantity": "1"
+    },
+    "top_bid": null,
+    "listing_date": null,
+    "supports_wyvern": false,
+    "rarity_data": null,
+    "transfer_fee": null,
+    "transfer_fee_payment_token": null,
+    "related_assets": [],
+    "orders": null,
+    "auctions": [],
+    "top_ownerships": [
+        {
+            "owner": {
+                "user": null,
+                "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/32.png",
+                "address": "0xb88f61e6fbda83fbfffabe364112137480398018",
+                "config": ""
+            },
+            "quantity": "1",
+            "created_date": "2019-08-17T03:07:10.527756+00:00"
+        }
+    ],
+    "ownership": null,
+    "highest_buyer_commitment": null,
+    "token_id": "1"
+}
+
+const moralisResponse = {
+    "token_address": "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
+    "token_id": "89831206571604020292044922253497274870202092698329182007733395779172644260551",
+    "transfer_index": [
+        22498031,
+        2,
+        21,
+        0
+    ],
+    "owner_of": "0xbcdca68a5fc87494066a0673e906163ad8575af0",
+    "block_number": "22498031",
+    "block_number_minted": "22115598",
+    "token_hash": "d00ad9240e8cd554cab40ed8257d694c",
+    "amount": "1",
+    "contract_type": "ERC1155",
+    "name": "OpenSea Collections",
+    "symbol": "OPENSTORE",
+    "token_uri": "https://api.opensea.io/api/v2/metadata/matic/0x2953399124F0cBB46d2CbACD8A89cF0599974963/0xc69aa93f640241201987e8409cbc41e04fc92bcc000000000000010000a98ac7",
+    "metadata": "{\"image\":\"https://i.seadn.io/gae/hPaLFLDKVXkqbUBuh4Gpde3DJjar56gwZmlf1p8H6hMNDRkRPqacSMqVCVG6G_cU3JJZsprXZF4I7wFWF2FLmUvU-WBrq9a7R_PL1w?w=500&auto=format\",\"name\":\"#Airdrop\",\"description\":\"MetaHero Identities are a collection of heroes, villains, and mutants native to the MetaHero Universe. The Core Collection consists of 146 fully-matched MetaHero concept characters, including two special mints, collaboratively designed by project creatives Chris Wahl and Odious. Generative MetaHero Identities (max supply of 9,678) can be minted at any time by redeeming MintPass #1, the signature NFT reservation system developed by Pixel Vault.\\n\\n\",\"external_link\":null,\"animation_url\":null,\"traits\":[]}",
+    "last_token_uri_sync": "2022-12-17T00:36:47.591Z",
+    "last_metadata_sync": "2022-12-17T00:36:49.902Z",
+    "minter_address": "ERC1155 tokens don't have a single minter"
+}
+
+const moralisMetadataResponse = {
+    "image": "https://i.seadn.io/gae/hPaLFLDKVXkqbUBuh4Gpde3DJjar56gwZmlf1p8H6hMNDRkRPqacSMqVCVG6G_cU3JJZsprXZF4I7wFWF2FLmUvU-WBrq9a7R_PL1w?w=500&auto=format",
+    "name": "#Airdrop",
+    "description": "MetaHero Identities are a collection of heroes, villains, and mutants native to the MetaHero Universe. The Core Collection consists of 146 fully-matched MetaHero concept characters, including two special mints, collaboratively designed by project creatives Chris Wahl and Odious. Generative MetaHero Identities (max supply of 9,678) can be minted at any time by redeeming MintPass #1, the signature NFT reservation system developed by Pixel Vault.\n\n",
+    "external_link": null,
+    "animation_url": null,
+    "traits": []
+}
+
+type AssetType = {
+    contract: string,
+    tokenId: string,
+    blockchain: string,
+    name: string,
+    imageUrl: string,
+    imageOriginal: string,
+    animationUrl: string,
+    animationOriginal: string,
+    info: {
+        name: string,
+        released: string,
+        collection: string,
+        creator: string,
+    }
+}
+
+interface Asset {
+    validateAsset(): AssetType
+}
+
+abstract class NftProvider {
+    public abstract getAsset(contract: string, tokenId: string): Asset
+
+    public abstract showAsset(contract: string, tokenId: string): string
+}
+
+class OpenseaAsset implements Asset {
+    private assetData
+
+    constructor(assetData) {
+        this.assetData = assetData
+    }
+
+    public validateAsset(): AssetType {
+        const asset = this.assetData;
+         return {
+             contract: asset.asset_contract.address,
+             tokenId: asset.token_id,
+             blockchain: 'eth',
+             name: asset.name,
+             imageUrl:
+                 asset.image_url ||
+                 asset.collection.large_image_url ||
+                 asset.asset_contract.image_url,
+             imageOriginal: asset.image_original_url,
+             animationUrl: asset.animation_url,
+             animationOriginal: asset.animation_original_url,
+             info: {
+                 name: asset.name ?? asset.token_id,
+                 creator: asset.creator.name || asset.creator.address,
+                 released: new Date(asset.collection.created_date).toLocaleDateString(),
+                 collection: asset.collection.name,
+             },
+         }
+    }
+}
+
+class MoralisAsset implements Asset {
+    private assetData
+
+    constructor(assetData) {
+        this.assetData = assetData
+    }
+
+    public validateAsset(): AssetType {
+        const asset = this.assetData
+        const metadata = asset.assetMetadata
+        return {
+            contract: asset.token_address,
+            tokenId: asset.token_id,
+            blockchain: 'matic',
+            name: metadata.name,
+            imageUrl: metadata.image,
+            imageOriginal: null,
+            animationUrl: metadata.animation_url || null,
+            animationOriginal: null,
+            info: {
+                name: metadata.name,
+                released: new Date(asset.last_token_uri_sync).toLocaleDateString(),
+                collection: asset.name,
+                creator: null,
+            },
+        }
+    }
+}
+
+class OpenseaProvider extends NftProvider {
+    public getAsset(contract: string, tokenId: string): Asset {
+        console.log(`get http request to https://api.opensea.io/api/v1/asset/${contract}/${tokenId}`);
+        return new OpenseaAsset(openseaResponse)
+    }
+
+    public showAsset(contract: string, tokenId: string) {
+        const asset = this.getAsset(contract, tokenId)
+        return 'Opensea validated asset: ' + JSON.stringify(asset.validateAsset());
+    }
+}
+
+class MoralisProvider extends NftProvider {
+    public getAsset(contract: string, tokenId: string): Asset {
+        console.log(`get http request to https://deep-index.moralis.io/api/v2/nft/${contract}/${tokenId}?chain=polygon&format=decimal`);
+        console.log('and get metadata from asset.tokenUri');
+        return new MoralisAsset({...moralisResponse, assetMetadata: moralisMetadataResponse});
+    }
+
+    public showAsset(contract: string, tokenId: string) {
+        const asset = this.getAsset(contract, tokenId)
+        return 'Moralis validated asset: ' + JSON.stringify(asset.validateAsset());
+    }
+}
+
+function testCode (provider: NftProvider, contract: string, tokenId: string) {
+    const asset = provider.showAsset(contract, tokenId)
+    console.log(asset);
+}
+
+console.log('opensea process:');
+testCode(new OpenseaProvider(), 'contract123', 'tokenId123')
+console.log('------------------');
+console.log('');
+console.log('');
+console.log('');
+
+console.log('moralis process:');
+testCode(new MoralisProvider(), 'contract456', 'tokenId456')
